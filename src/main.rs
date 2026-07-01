dfe251978a462cfd4035cca5c38d8a24b15d469b
bad_indentation <ulrichjohnsen314@gmail.com>
2026-07-01 15:31:55 -0400

    feat: program entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+//! # HangStop
+//! (c) 2026 by bad_indentation
+//!
+//! Program entry point
+
+use std::error::Error;
+
+use hangstop::{Config, run};
+use clap::Parser;
+
+fn main() -> Result<(), Box<dyn Error>> {
+   run(Config::parse()) 
 }
