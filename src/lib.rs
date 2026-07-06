0e2e80db417b17a67fe0a746d3b3a9baea7ba134
bad_indentation <ulrichjohnsen314@gmail.com>
2026-07-06 18:48:50 -0400

    refactor: remove dead regex-related code

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@ use std::error::Error;
 use std::fmt::Display;
 use std::iter::zip;
 
-// use regex::Regex;
 use clap::Parser;
 
 pub const ALL_WORDS: &str = include_str!("../public/wordlist.txt");
@@ -35,54 +34,6 @@ impl GameData<'_> {
             all_excluded,
         }
     }
-}
-
-/// Constructs a regular expression, as an owned String, given the game state
-/// and the incorrectly guessed characters. If `state` contains any characters
-/// other than `?` and ASCII letters or `forbidden` contains any characters
-/// other than ASCII letters, an `Err()` variant will be returned.
-///
-/// # Arguments
-/// `state`: The correctly guessed letters and blanks (represented by `?`),
-/// in the order that they would appear in the partially revealed answer.
-/// For instance, in a game of Hangman, if the secret word is `hello` and
-/// the other player has guessed `h` and `l`, the resulting state would look
-/// like `h?ll?`. Note that these letters are also excluded by the regex
-/// because the rules of Hangman dictate that once a letter is guessed,
-/// all occurences of that letter must be uncovered.
-///
-/// `forbidden`: Any incorrectly guessed letters.
-///
-pub fn build_regex(state: &str, forbidden: &str) -> Result<String, &'static str> {
-    let state = state.to_lowercase();
-    let forbidden = forbidden.to_lowercase();
-
-    let mut excluded = state.replace("?", "");
-
-    if forbidden
-        .matches(|letter: char| !letter.is_alphabetic())
-        .count()
-        > 0
-    {
-        return Err("forbidden letters must only contain alphabetic characters.");
-    }
-
-    excluded.push_str(&forbidden.to_lowercase());
-
-    let mut re = String::from("^");
-
-    for letter in state.chars() {
-        match letter {
-            '?' =>  if !excluded.is_empty() {
-                re.push_str(&format!("[^{excluded}]")); 
-            } else { re.push('.'); },
-            'a'..='z' => re.push(letter),
-            _ => return Err("game state must only contain alphabetic characters or ?s."),
-        }
-    }
-
-    re.push('$');
-    Ok(re)
 }
 
 /// Returns `true` if the word contains all of the letters uncovered in `state`,
@@ -321,30 +272,6 @@ mod tests {
     use std::collections::HashSet;
 
     use super::*;
-
-    #[test]
-    #[ignore = "regex no longer used"]
-    fn test_build_regex() {
-        let re = build_regex("A???e", "krbg").unwrap();
-        assert_eq!(re, "^a[^aekrbg][^aekrbg][^aekrbg]e$".to_string());
-    }
-
-    #[test]
-    #[ignore = "regex no longer used"]
-    fn test_build_regex_with_invalid_characters() {
-        let re = build_regex("*(*H)", "kjd");
-        assert!(re.is_err());
-
-        let re = build_regex("h?ll?", "(*&j)");
-        assert!(re.is_err());
-    }
-
-    #[test]
-    #[ignore = "regex no longer used"]
-    fn test_build_regex_none_forbidden() {
-        let re = build_regex("?????", "").unwrap();
-        assert_eq!(re, "^.....$".to_string());
-    }
 
     #[test]
     fn test_matches_constraints() {
